0c75f14bc4230715acdb75883819cda8b8ebcf40
Ahmed Bahy <ahmedbahy2026@gmail.com>
2024-12-15 20:36:57 +0200

    Add MemBlock and Process

--- src/main/memoryManagement/Process.java
+++ src/main/memoryManagement/Process.java
@@ -1,5 +1,23 @@
 package main.memoryManagement;
 
+import java.util.Random;
+
 public class Process {
+  private int size;
+	private int id;
+	private int timeout;
+	private MemBlock memBlcok;
+	
+	
+	public Process(int id){
+		Random random = new Random();
+		this.id = id;
+		timeout = random.nextInt(20) + 1;
+		memBlcok = null;
+	}
+
+  public String toString(){
+    return "ProcessId: "+ this.id + "ProcessSize: "+ this.size;
+  }
   
 }
